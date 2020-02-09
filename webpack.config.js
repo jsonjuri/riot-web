9f49181bd85a9001627346508f9d3f05ff23fdab
JsonJuri <jsonjuri@gmail.com>
2020-02-09 21:54:11 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,9 @@ module.exports = (env, argv) => {
 
             // CSS themes
             "theme-light": "./node_modules/matrix-react-sdk/res/themes/light/css/light.scss",
-            "theme-dark": "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss",
-            "theme-light-custom": "./node_modules/matrix-react-sdk/res/themes/light-custom/css/light-custom.scss",
-            "theme-dark-custom": "./node_modules/matrix-react-sdk/res/themes/dark-custom/css/dark-custom.scss",
+            //"theme-dark": "./node_modules/matrix-react-sdk/res/themes/dark/css/dark.scss",
+            //"theme-light-custom": "./node_modules/matrix-react-sdk/res/themes/light-custom/css/light-custom.scss",
+            //"theme-dark-custom": "./node_modules/matrix-react-sdk/res/themes/dark-custom/css/dark-custom.scss",
         },
 
         optimization: {
